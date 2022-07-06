44e365bbfc41f529d22857ebb8acf6f61345ae63
Omkar <omkarbaacha@foreignadmits.com>
2022-07-07 01:15:18 +0530

    Authentication refactored

--- 01 Blogging Site/src/middleware/middleware.js
+++ 01 Blogging Site/src/middleware/middleware.js
@@ -5,31 +5,45 @@ const isValidObjectId = function (objectId) {
 };
 const blogModels= require("../models/blogModel.js")
 
-const login = async (req,res,next)=>{
+const authentication = async (req,res,next)=>{
 
     try{
 
     // take token from client 
-    let token = req.headers["x-Api-key"]
+    let token1 =  req.headers['authorization']
 
-    if( token == undefined ) { token= req.headers["x-api-key"] }
+    if( token1 == undefined ) { token= req.headers["Authorization"] }
 
-    //If no token is present in the request header return error
-    if (!token) return res.status(401).send({ status: false, msg: "Token must be present" });
+        // if No token found then send error
+        if (!token1) {
+          return res.status(401).send({ status: false, msg: "Authentication token is required" })
+        }
 
-    //if token is present then decode the token
-    let decodedToken = jwt.verify(token,"Blogging-Site")
-    
-    // Check Decoded token is here or not
-    if(!decodedToken) return res.status(401).send({ status : false, msg : "Token is Not Present"})
+            // Split that Bearer Token 
+          let token = token1.split(' ')[1]
 
-    req.decodedToken = decodedToken 
-    // if Everything is ok then we head towards Api's
-    next();
 
-}catch(err)
+    // Now Verify that token in Decoded Token
+    jwt.verify(token, "Blogging-Site", { ignoreExpiration: true }, function (err, decoded) {
+      if (err) { 
+        return res.status(400).send({ status: false, meessage: "token invalid" }) }
+      else {
+        if (Date.now() > decoded.exp * 1000) {
+          return res.status(401).send({ status: false, msg: "Session Expired", });
+        }
+       
+       // Store Decoded Token User Id into request header named as userId
+       req.decodedToken = decoded;
+       
+       // Now Simply Next the flow 
+       next();
+      }
+    });
+
+}
+catch(err)
 {
-res.status(401).send({ status: false, err : "Token is Invalid" })
+    res.status(500).send({ msg: "Error", error: err.message })
 }
 }
 
@@ -64,4 +78,4 @@ const AuthorizationById = async  (req, res, next) =>{
 };
 
 
-module.exports={ AuthorizationById,login}
+module.exports={ AuthorizationById,authentication}
