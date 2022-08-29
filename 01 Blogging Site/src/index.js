e7136ba6d99ae8fcd01fb6f01cad5c445dc7b7dd
Omkar <omkarbacha25@gmail.com>
2022-08-29 15:23:51 +0530

    Swagger added

--- 01 Blogging Site/src/index.js
+++ 01 Blogging Site/src/index.js
@@ -8,10 +8,37 @@ const connectDB = require('./db/connect')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+//Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+//extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 app.use(cors())
 app.use("/", route);
 const port = process.env.PORT || 3000
 
+
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+  app.use(express.json());
+  app.use(helmet());
+  app.use(cors());
+  app.use(xss());
+  app.set('trust proxy', 1);
+  app.use(
+    rateLimiter({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+      standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+      legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    })
+  );
 
 const start = async () => {
   try {
